fcb0b91d2f894aff3905995ff508085f483402e4
Jim Rowe <james.rowe@tpagency.com>
2022-09-14 17:14:18 +0100

    testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Vimeo } from ("vimeo");
+const Vimeo = require("vimeo").Vimeo;
 
 module.exports = {
 	init(config) {
@@ -10,29 +10,60 @@ module.exports = {
 
 		return {
 			upload(file) {
+				console.log("TEST FILE->");
+				console.dir(file);
+
 				return new Promise((resolve, reject) => {
-					client.upload(file.buffer,
-						{
-							name: file.hash,
-							description: file.alternativeText,
-						},
-						() => {
+					client.request({
+						method: "POST",
+						path: "/me/videos",
+						name: file.hash,
+						folder_uri: config.folderId,
+						description: file.alternativeText,
+						upload: {
+							approach: 'tus',
+							size: file.length
+						}
+					},
+					function(error, body, status_code, headers) {
 							// complete
-							console.log("complete");
-							console.dir(this);
+							console.log("error:"+error);
+							console.log("status_code:"+status_code);
+
+							// console.dir(body);
+							// console.dir(headers);
 							// file.url = res.data.link;
+
+							// now send the actual data
+							client.request({
+								method: "PATCH",
+								headers: {
+									"Tus-Resumable": "1.0.0",
+									"Upload-Offset": "0",
+									"Content-Type": "application/offset+octet-stream"
+								},
+								path: body.upload.upload_link,
+							},
+							function(error, body, status_code, headers) {
+
+							});
+
+							// check progress
+
 							resolve();
-						},
-						(bytesUploaded, bytesTotal) => {
-							// progress
-							var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
-							console.log(bytesUploaded, bytesTotal, percentage + "%")
-						},
-						(err) => {
-							// error
-							console.log(err);
-							reject();
 					});
+
+					// ,
+					// 	(bytesUploaded, bytesTotal) => {
+					// 		// progress
+					// 		var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
+					// 		console.log(bytesUploaded, bytesTotal, percentage + "%")
+					// 	},
+					// 	(err) => {
+					// 		// error
+					// 		console.log(err);
+					// 		reject();
+					// });
 				});
 			},
 			delete(file) {
@@ -43,7 +74,7 @@ module.exports = {
 							method: "DELETE",
 							path: "/videos/" + str.split("/")[str.split("/").length - 1],
 						},
-						function (error, body, status_code, headers) {
+						function(error, body, status_code, headers) {
 							if (error) {
 								console.log(error);
 							}
