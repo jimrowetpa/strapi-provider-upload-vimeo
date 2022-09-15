d8beef9813fb84b694bb2960c941b907d3e10f10
Jim Rowe <james.rowe@tpagency.com>
2022-09-15 17:04:08 +0100

    testing

--- index.js
+++ index.js
@@ -1,85 +1,218 @@
-const Vimeo = require("vimeo").Vimeo;
+const axios = require("axios");
 
 module.exports = {
 	init(config) {
-		var client = new Vimeo(
-			config.clientId,
-			config.clientSecret,
-			config.accessToken
-		);
+
+		const client = axios.create({
+			baseURL: `https://api.vimeo.com/`,
+			headers: {
+				'Authorization': `bearer ${config.accessToken}`
+			}
+		});
+
+		const startTusUpload = function(file) {
+
+			return new Promise((resolve, reject) => {
+
+				// create a video
+				return client({
+					method: 'POST',
+					url: 'me/videos',
+					headers: {
+						'Content-Type': 'application/json',
+						'Accept': 'application/vnd.vimeo.*+json;version=3.4'
+					},
+					data: {
+						'name': file.hash,
+						'description': file.caption,
+						'folder_uri': `/folders/${config.folderId}`,
+						'upload': {
+							'approach': 'tus',
+							'size': `${file.buffer.length}`
+						}
+					}
+				}).then(function(response) {
+
+					resolve(response.data);
+
+				}).catch(function (error) {
+					console.log("error starting upload: "+error.response.data.error);
+					if (error.response.data.invalid_parameters) {
+						console.dir(error.response.data.invalid_parameters);
+					} else if (error.response.data) {
+						console.dir(error.response.data);
+					}
+					reject();
+				});
+
+			});
+		}
+
+		const sendTusData = function(fileUrl, file) {
+
+			return new Promise((resolve, reject) => {
+
+				// send data using patch
+				client({
+					method: 'PATCH',
+					url: fileUrl,
+					headers: {
+						'Tus-Resumable': '1.0.0',
+						'Upload-Offset': '0',
+						'Content-Type': 'application/offset+octet-stream'
+					},
+					data: file.buffer
+				}).then(function(response) {
+
+					// set return data directly on file
+					file
+
+					resolve();
+
+				}).catch(function (error) {
+					console.log("error sending upload: "+error.response.data.error);
+					if (error.response.data.invalid_parameters) {
+						console.dir(error.response.data.invalid_parameters);
+					} else if (error.response.data) {
+						console.dir(error.response.data);
+					}
+					reject();
+				});
+			});
+		}
+
+		const getTranscodedVideo = function(id, maxattempts) {
+
+			console.log("getTranscodedVideo:"+id);
+
+			return new Promise((resolve, reject) => {
+
+				if (maxattempts <= 0) {
+					reject();
+					return;
+				}
+
+				client({
+					method: 'GET',
+					url: `videos/${id}`
+				}).then(function(response) {
+
+					// set return data directly on file
+					// console.log("getTranscodedVideo");
+					// console.dir(response.data.transcode);
+
+					// unless we start uploading in chunks we always upload in one go,
+					// so no need to check progress
+
+					// check transcode status
+					let status = response.data.transcode.status;
+					console.log("status:"+status+"   ("+maxattempts+")");
+					if (status == "available") {
+						resolve(response.data);
+					} else {
+						setTimeout(() => {
+							return getTranscodedVideo(id, maxattempts - 1);
+						}, 1000);
+					}
+
+				}).catch(function (error) {
+					console.log("error getting transcoded upload: "+error);
+				});
+
+			});
+		}
+
+		const idFromUri = function(uri) {
+
+			const uriParts = uri.split('/');
+			const id = uriParts[uriParts.length - 1];
+			return id;
+		}
+
+		const updateFileData = function(file, data) {
+
+			const id = idFromUri(data.uri);
+
+			file.provider_metadata = {
+				url: data.link,
+				player_embed_url: data.player_embed_url,
+				width: data.width,
+				height: data.height,
+				embed: data.embed.html,
+				// pictures: data.pictures,
+				id: id
+			};
+
+			file.width = data.width;
+			file.height = data.height;
+
+			// preview url
+			// if (data.pictures.sizes.length > 3) {
+			// 	file.previewUrl = data.pictures.sizes[3].link;
+			// } else if (startData.pictures.sizes.length > 0) {
+			// 	file.previewUrl = data.pictures.sizes[data.pictures.sizes.length - 1].link;
+			// }
+
+			file.url = data.link;
+		}
 
 		return {
 			upload(file) {
-				console.log("TEST FILE->");
-				console.dir(file);
 
 				return new Promise((resolve, reject) => {
-					client.request({
-						method: "POST",
-						path: "/me/videos",
-						name: file.hash,
-						folder_uri: config.folderId,
-						description: file.alternativeText,
-						upload: {
-							approach: 'tus',
-							size: file.length
-						}
-					},
-					function(error, body, status_code, headers) {
-							// complete
-							console.log("error:"+error);
-							console.log("status_code:"+status_code);
 
-							// console.dir(body);
-							// console.dir(headers);
-							// file.url = res.data.link;
+					startTusUpload(file).then(function(startData) {
 
-							// now send the actual data
-							client.request({
-								method: "PATCH",
-								headers: {
-									"Tus-Resumable": "1.0.0",
-									"Upload-Offset": "0",
-									"Content-Type": "application/offset+octet-stream"
-								},
-								path: body.upload.upload_link,
-							},
-							function(error, body, status_code, headers) {
+						updateFileData(file, startData);
 
-							});
+						sendTusData(startData.upload.upload_link, file).then(function() {
 
-							// check progress
+							// poll for progress and update fields
+							// can't do this - it takes too long, have to reload
+							// where we are using the videos
+							// getTranscodedVideo(file.provider_metadata.id, 300).then(function() {
+								resolve();
+							// }).catch(function (error) {
+							// 	reject();
+							// });
 
-							resolve();
+						}).catch(function (error) {
+							console.log("ERROR");
+							console.dir(error);
+							reject();
+						});
+					}).catch(function (error) {
+						console.log("ERROR");
+						console.dir(error);
+						reject();
 					});
-
-					// ,
-					// 	(bytesUploaded, bytesTotal) => {
-					// 		// progress
-					// 		var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
-					// 		console.log(bytesUploaded, bytesTotal, percentage + "%")
-					// 	},
-					// 	(err) => {
-					// 		// error
-					// 		console.log(err);
-					// 		reject();
-					// });
 				});
+
 			},
 			delete(file) {
-				if (file && file.provider_metadata && file.provider_metadata.link) {
-					str = file.provider_metadata.link;
-					client.request(
-						{
-							method: "DELETE",
-							path: "/videos/" + str.split("/")[str.split("/").length - 1],
-						},
-						function(error, body, status_code, headers) {
-							if (error) {
-								console.log(error);
+				if (file && file.provider_metadata && file.provider_metadata.id) {
+
+					return new Promise((resolve, reject) => {
+
+						// create a video
+						return client({
+							method: 'DELETE',
+							url: `videos/${file.provider_metadata.id}`
+						}).then(function(response) {
+
+							resolve();
+
+						}).catch(function (error) {
+							console.log("error deleting upload: "+error.response.data.error);
+							if (error.response.data.invalid_parameters) {
+								console.dir(error.response.data.invalid_parameters);
+							} else if (error.response.data) {
+								console.dir(error.response.data);
 							}
-						}
-					);
+							reject();
+						});
+
+					});
 				}
 			},
 		};
